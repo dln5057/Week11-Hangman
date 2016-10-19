b4f64451b211bebc3b69752c07683eee4970708f
Donnie Nowels <djnowels11@gmail.com>
2016-10-19 17:42:18 -0400

    added more code to word.js

--- word.js
+++ word.js
@@ -1,20 +1,28 @@
 // require your letter objects
-
+var letter = require('./letter.js');
 var Word = function(wrd){
 // property to store the string wrd
-
+	this.chosenword = wrd,
 // a collection of letter objects
-
+	this.lettersInWord = [],
 // property is the word found?
+	this.found = false,
 
 	this.getLets = function() {
 // populate the collection above with new Letter objects
+			word.lettersInWord.push(Letter)
+
 	};
 	
 	//found the current word
 	this.didWeFindTheWord = function() {
 		//sets this.found in the word object to true or false if all letter objects have a true value in their appear property
-		});
+		if(letter.appear == true){
+			this.found = true;
+		}else{
+			this.found = false;
+		}
+	
 
 		return this.found;
 	};
@@ -22,15 +30,21 @@ var Word = function(wrd){
 	this.checkIfLetterFound = function(guessLetter) {
 
 // iterate through the collection of letter Objects
-
+	for(var i = 0; i < this.lettersInWord.lenght){
+		if(guessLetter == this.lettersInWord[i]){	
+			
+		}
+	}
 // if guessLetter matches Letter property, the letter object should be shown
 		return whatToReturn;
 	};
 
 	this.wordRender = function() {
-// render the word based on if letters are found or ot found
+// render the word based on if letters are found or not found
+
 		return str;
 	};
 }
 
 // export to use in main.js
+module.exports = word;
